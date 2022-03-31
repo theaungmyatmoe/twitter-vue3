508073657220f8c52d9946c7054a47308b2288c6
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-31 21:48:43 +0630

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ user.username }} - {{ full_name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        first_name: 'Aung',
+        last_name: 'Myat Moe',
+        age: 19,
+        username: 'amm834',
+      }
+    }
+  },
+  computed: {
+    full_name() {
+      return `${this.user.first_name} ${this.user.last_name}`
+    }
+  }
+}
+</script>

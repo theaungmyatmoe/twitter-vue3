e38aaf45879685c7812ef37c366b216216fc41dd
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-04-01 00:05:56 +0630

    Refactoring components
    Signed-off-by: Aung Myat Moe <aungmyatmoe834@gmail.com>

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary text-light">
+    <div class="container">
+      <a class="navbar-brand text-light" href="#">Tweeter</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <!--            <li class="nav-item">-->
+          <!--              <a class="nav-link active" aria-current="page" href="#">Home</a>-->
+          <!--            </li>-->
+        </ul>
+        <div class="d-flex">
+          <h5 class="fw-bold font-monospace"> @{{ user.username }}</h5>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,9 +1,10 @@
 <script>
 import TweetItem from "./TweetItem.vue";
+import NavBar from "./NavBar.vue";
 
 export default {
   name: 'UserProfile',
-  components: {TweetItem},
+  components: {NavBar, TweetItem},
   data() {
     return {
       tweet: {
@@ -69,60 +70,63 @@ export default {
 
 
 <template>
-  <div class="container">
-    <div class="row my-3">
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <!--            Profile info-->
-            <div>
-              <h3 class="display-6 fw-bold">@{{ user.username }}</h3>
-              <span v-if="user.isAdmin" class="badge bg-success"> Admin </span>
-              <span class="badge bg-primary ms-3">{{ followers }} Followers</span>
+  <main>
+    <NavBar :user="user"/>
+    <div class="container">
+      <div class="row my-3">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <!--            Profile info-->
+              <div>
+                <h3 class="display-6 fw-bold">@{{ user.username }}</h3>
+                <span v-if="user.isAdmin" class="badge bg-success"> Admin </span>
+                <span class="badge bg-primary ms-3">{{ followers }} Followers</span>
+              </div>
+            </div>
+          </div>
+
+          <!--             New tweet-->
+          <div class="card mt-3">
+            <div class="card-header">
+              New Tweet
+              ( {{ tweetCount }}/160)
+            </div>
+            <div class="card-body">
+              <form @submit.prevent="postTweet">
+                <div class="mb-3">
+                  <label for="newTweet" class="form-label fw-bold"
+                  >Tweet</label>
+                  <textarea id="newTweet" rows="4" class="form-control"
+                            v-model="tweet.content"
+                            :class="{'is-invalid' : tweetCount > 160}"
+                  ></textarea>
+                </div>
+                <div class="mb-3">
+                  <select class="form-select" v-model="tweet.tweetType">
+                    <option
+                        :value="option.value"
+                        v-for="(option,index) in tweetTypes"
+                        :key="index"
+                    > {{ option.name }}
+                    </option>
+                  </select>
+                </div>
+                <button type="submit" class="btn btn-primary w-100  ">Tweet</button>
+              </form>
             </div>
           </div>
         </div>
-
-        <!--             New tweet-->
-        <div class="card mt-3">
-          <div class="card-header">
-            New Tweet
-            ( {{ tweetCount }}/160)
-          </div>
-          <div class="card-body">
-            <form @submit.prevent="postTweet">
-              <div class="mb-3">
-                <label for="newTweet" class="form-label fw-bold"
-                >Tweet</label>
-                <textarea id="newTweet" rows="4" class="form-control"
-                          v-model="tweet.content"
-                          :class="{'is-invalid' : tweetCount > 160}"
-                ></textarea>
-              </div>
-              <div class="mb-3">
-                <select class="form-select" v-model="tweet.tweetType">
-                  <option
-                      :value="option.value"
-                      v-for="(option,index) in tweetTypes"
-                      :key="index"
-                  > {{ option.name }}
-                  </option>
-                </select>
-              </div>
-              <button type="submit" class="btn btn-primary w-100  ">Tweet</button>
-            </form>
-          </div>
+        <div class="col-md-8">
+          <TweetItem
+              v-for="(tweet,index) in user.tweets"
+              :key="index"
+              :username="user.username"
+              :tweet="tweet"
+              @favorite="toggleFavorite"
+          />
         </div>
       </div>
-      <div class="col-md-8">
-        <TweetItem
-            v-for="(tweet,index) in user.tweets"
-            :key="index"
-            :username="user.username"
-            :tweet="tweet"
-            @favorite="toggleFavorite"
-        />
-      </div>
     </div>
-  </div>
+  </main>
 </template>

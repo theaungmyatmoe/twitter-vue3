846d9de15433c95fbaf26cf1fd2b589a07bb91b7
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-31 22:06:33 +0630

    Watcher, Life Cycle Hooks and computed properties
    Signed-off-by: Aung Myat Moe <aungmyatmoe834@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    {{ user.username }} - {{ full_name }}
+    <p> {{ user.username }} - {{ full_name }} </p>
+    <p><strong> Followers : </strong> {{ followers }} </p>
+    <p>
+      <button @click="followUser">Follow</button>
+    </p>
   </div>
 </template>
 
@@ -8,6 +12,7 @@
 export default {
   data() {
     return {
+      followers: 0,
       user: {
         first_name: 'Aung',
         last_name: 'Myat Moe',
@@ -16,10 +21,26 @@ export default {
       }
     }
   },
+  mounted() {
+    this.followUser()
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (newFollowerCount > oldFollowerCount) {
+        console.log('You gained a follower!')
+      }
+    },
+  },
   computed: {
     full_name() {
       return `${this.user.first_name} ${this.user.last_name}`
     }
-  }
+  },
+
 }
 </script>
